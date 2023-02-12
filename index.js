af8f2fc9db742f8717826272390734af870dd04a
Tony Tong <tao.tong@auditoria.ai>
2023-02-12 12:26:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.set('views', 'web')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'web')));
 
-app.use('/', (req, res, next) => {
+// catch all 404 errors and send to 404.html
+app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'web', '404.html'));
 });
 
